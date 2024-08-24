2eaae77adb1c059559e3bd9b152033a48d79279d
Yong Yong <147980519+yadada-wA@users.noreply.github.com>
2024-08-24 23:35:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,16 @@ const imgEl = document.getElementById('Paint');
 const loader = document.getElementById('loader');
 const btn = document.getElementById('bth');
 
-const srcArray = [
-                    'IMG_0691.PNG',
-                    'IMG_0692.PNG',
-                    'IMG_0693.PNG',
-                    'IMG_0694.PNG',
-                    'IMG_0695.PNG',
-                    'IMG_0696.PNG',
-                    'IMG_0697.PNG'
-                ];
 
-//const srcArray = [
-//                    '/assets/pic/IMG_0691.PNG',
-//                    '/assets/pic/IMG_0692.PNG',
-//                    '/assets/pic/IMG_0693.PNG',
-//                    '/assets/pic/IMG_0694.PNG',
-//                    '/assets/pic/IMG_0695.PNG',
-//                   '/assets/pic/IMG_0696.PNG',
-//                    '/assets/pic/IMG_0697.PNG'
-//                ];
+const srcArray = [
+                    '/assets/pic/IMG_0691.PNG',
+                    '/assets/pic/IMG_0692.PNG',
+                    '/assets/pic/IMG_0693.PNG',
+                    '/assets/pic/IMG_0694.PNG',
+                    '/assets/pic/IMG_0695.PNG',
+                    '/assets/pic/IMG_0696.PNG',
+                    '/assets/pic/IMG_0697.PNG'
+                ];
 
 
 btn.addEventListener
