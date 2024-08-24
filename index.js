84ecdc883ef1feca524afeeeb476ee25cc7c5222
Yong Yong <147980519+yadada-wA@users.noreply.github.com>
2024-08-24 22:43:07 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const imgEl = document.getElementById('Paint');
+const loader = document.getElementById('loader');
+const btn = document.getElementById('bth');
+
+
+const srcArray = [
+                    '/assets/pic/IMG_0691.PNG',
+                    '/assets/pic/IMG_0692.PNG',
+                    '/assets/pic/IMG_0693.PNG',
+                    '/assets/pic/IMG_0694.PNG',
+                    '/assets/pic/IMG_0695.PNG',
+                    '/assets/pic/IMG_0696.PNG',
+                    '/assets/pic/IMG_0697.PNG'
+                ];
+
+
+btn.addEventListener
+('click', () => {
+
+        { randomImg(); function randomImg() 
+        {   const randomIndex = Math.floor(Math.random()*srcArray.length);
+            imgEl.src = srcArray[randomIndex];}}
+   
+});
+
