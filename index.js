f2e29687424c6e5b32df7ccdc4994e20e60e514f
Yong Yong <147980519+yadada-wA@users.noreply.github.com>
2024-08-26 13:38:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,24 +2,7 @@ const imgEl = document.getElementById('Paint');
 const btn = document.getElementById('bth');
 
 
-const srcArray = [
-                    'assets/pic/IMG_0691.PNG',
-                    'assets/pic/IMG_0692.PNG',
-                    'assets/pic/IMG_0693.PNG',
-                    'assets/pic/IMG_0694.PNG',
-                    'assets/pic/IMG_0695.PNG',
-                    'assets/pic/IMG_0696.PNG',
-                    'assets/pic/IMG_0697.PNG'
-                ];
-
-function randomImg() {
-  const randomIndex = Math.floor(Math.random() * srcArray.length);
-  imgEl.src = srcArray[randomIndex];
-}
-
-btn.addEventListener('click', () => {
-  randomImg();
-});
+let srcArray = [];
 
 function preload() {
   for (var i =0; i< arguments.length; i++){
@@ -37,3 +20,13 @@ preload(
                     'assets/pic/IMG_0696.PNG',
                     'assets/pic/IMG_0697.PNG'
 )
+
+function randomImg() {
+  const randomIndex = Math.floor(Math.random() * srcArray.length);
+  imgEl.src = srcArray[randomIndex];
+}
+
+btn.addEventListener('click', () => {
+  randomImg();
+});
+
