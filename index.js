72d6b264b56c9707d98c0888f0fd44e366f461f0
Yong Yong <147980519+yadada-wA@users.noreply.github.com>
2024-08-26 14:06:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,28 +2,34 @@ const imgEl = document.getElementById('Paint');
 const btn = document.getElementById('bth');
 
 
-let srcArray = [];
-
-function preload() {
-  for (var i=0; i< arguments.length; i++{
-    srcArray[i] = new Image();
-    srcArray[i] = preload.argument[i];
-  }
-}
-
-preload(
-                    'assets/pic/IMG_0691.PNG',
+let srcArray = [    'assets/pic/IMG_0691.PNG',
                     'assets/pic/IMG_0692.PNG',
                     'assets/pic/IMG_0693.PNG',
                     'assets/pic/IMG_0694.PNG',
                     'assets/pic/IMG_0695.PNG',
                     'assets/pic/IMG_0696.PNG',
-                    'assets/pic/IMG_0697.PNG'
-)
+                    'assets/pic/IMG_0697.PNG'];
+
+//function preload() {
+//  for (var i=0; i< arguments.length; i++{
+//   srcArray[i] = new Image();
+//   srcArray[i] = preload.argument[i];
+//  }
+//}
+
+//preload( 
+//                   'assets/pic/IMG_0691.PNG',
+//                    'assets/pic/IMG_0692.PNG',
+//                    'assets/pic/IMG_0693.PNG',
+//                    'assets/pic/IMG_0694.PNG',
+//                    'assets/pic/IMG_0695.PNG',
+//                    'assets/pic/IMG_0696.PNG',
+//                    'assets/pic/IMG_0697.PNG'
+//)
 
 function randomImg() {
   const randomIndex = Math.floor(Math.random() * srcArray.length);
-  imgEl.src = srcArray[randomIndex].src;
+  imgEl.src = srcArray[randomIndex];
 }
 
 btn.addEventListener('click', () => {
